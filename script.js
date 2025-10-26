0b761ddde6ecfe4437878b5f7afd0370adfea573
Shifa Sodagar <codewithshifa08@gmail.com>
2025-10-26 09:52:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-//JS program to display the current time ,day, date
-let today = new Date(); //getting new date and time
-let day = today.getDay();//get the day and week where 0 is sunday and 6 is saturday0-6
-let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let date = today.getDate();
-let month = today.getMonth() + 1; //month start from 0
-let year = today.getFullYear();
-console.log(` Today is  ${daylist[day]}, ${date} - ${month} - ${year} `);
-let hour = today.getHours();
-let minutes = today.getMinutes();
-let seconds = today.getSeconds();
-let prepand = (hour >= 12) ? 'PM ' : ' AM';
-hour = (hour >= 12) ? hour - 12 : hour; //convert 24hour format to 12 hour format
-if (hour === 0 && prepand === 'PM') {
-    if (minute === 0 && second === 0) {
-        hour = 12
-        prepand = 'noon'
-    }
-    else {
-        hour = 12
-        prepand = 'PM';
-    }
-}
-if (hour === 0 && prepand === 'AM') {
-    if (minute === 0 && second === 0) {
-        hour = 12
-        prepand = 'Midnight'
-    }
-    else {
-        hour = 12
-        prepand = 'AM';
-    }
-}
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        } 
+        else if (e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
 
-console.log("Current time : " + hour + prepand + " : " + minutes + ' : ' + seconds);
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '%'){
+            string = string + '/100'
+            input.value = string;
+           
+
+        }
+
+
+        
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
